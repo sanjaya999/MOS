19d1e9166bc568f6d90279ba1850592bdb83e8a2
san <poudelsanjaya12@gmail.com>
2025-08-29 09:01:53 +0545

    Add types header file with definitions for video memory and screen dimensions

---
+++ kernel/include/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+
+#define VIDEO_MEMORY 0xB8000
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+
+// Colors for terminal
+#define WHITE_ON_BLACK 0x0F
+#define GREEN_ON_BLACK 0x0A
+#define RED_ON_BLACK   0x0C
+
+#endif
